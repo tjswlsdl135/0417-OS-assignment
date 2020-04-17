bda878c8a931197a60590efd6a9a4fb5fcc095c8
SunJin Park <tjswlsdl135@naver.com>
2020-04-17 10:51:42 +0900

    1~8 times table

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "1단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 1 << " x " << i << " = " << 1 * i << endl;
+	}
+	cout << "2단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 2 << " x " << i << " = " << 2 * i << endl;
+	}
+	cout << "3단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 3 << " x " << i << " = " << 3 * i << endl;
+	}
+	cout << "4단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 4 << " x " << i << " = " << 4 * i << endl;
+	}
+	cout << "5단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 5 << " x " << i << " = " << 5 * i << endl;
+	}
+	cout << "6단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 6 << " x " << i << " = " << 6 * i << endl;
+	}
+	cout << "7단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 7 << " x " << i << " = " << 7 * i << endl;
+	}
+	cout << "8단" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << 8 << " x " << i << " = " << 8 * i << endl;
+	}
+	cout << "9단" << endl;
+
+}
